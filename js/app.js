b20183d088557fbfe968472ddfdc8134dc2c54dd
杨杰斌 <yangjb@chinanetcenter.com>
2017-06-02 17:32:47 +0800

    directive scope

--- js/app.js
+++ js/app.js
@@ -1,53 +1,12 @@
 (function () {
-    var app=angular.module('course',[]);
+    var app=angular.module('app',['course']);
 
-    // app.controller("courseController",function () {
-    //     this.courseList=courseList;
-    // });
-    //
-    // app.controller('formController',function () {
-    //     this.courseList=courseList;
-    //     this.addCourse=function (course) {
-    //         courseList.push(course);
-    //         this.course={};
-    //     }
-    // });
-    app.directive("courseList",function () {
-       return{
-           restrict:'E',
-           templateUrl:'course-list.html',
-           controller:function () {
-               this.courseList=courseList;
-           },
-           controllerAs:"coursectrl"
-       }
-    });
-    app.directive("courseForm",function () {
-        return{
-            restrict:'E',
-            templateUrl:'course-form.html',
-            controller:function () {
-                this.courseList=courseList;
-                this.addCourse=function (course) {
-                    courseList.push(course);
-                    this.course={};
-                }
-            },
-            controllerAs:"formctrl"
-        }
-    });
+    app.controller('MainController',['$http','$scope',function ($http,$scope) {
+        $scope.courseList=[];
+        $http.get('/angular-project/data/data.json')
+            .then(function (data) {
+                $scope.courseList=data.data;
 
-    var courseList=[{
-        "name":"Shaping Up With AngularJS",
-        "category":"javascript",
-        "image":"image/1.jpg",
-        "des":"Learn to extend HTML's syntax with AngularJS to create dynamic web applications.",
-        "time":"5.0"
-    },{
-        "name":"Staying Sharp With AngularJS",
-        "category":"javascript",
-        "image":"image/3.jpg",
-        "des":"Use AngularJS to create flexible directives and reusable services for a modular and speedy app!",
-        "time":"6.0"
-    }];
+            });
+    }]);
 })();
