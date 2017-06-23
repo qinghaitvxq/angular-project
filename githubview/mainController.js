fae8ee182a19d891e45a4b76f2146e9e73193286
qinghaitvxq <598616202@qq.com>
2017-06-23 11:11:28 +0800

    add github viwe fun

--- githubview/mainController.js
+++ githubview/mainController.js
@@ -1,13 +1,52 @@
 /**
  * Created by yunxi on 2017/6/21.
  */
-
 (function () {
-    var mainController=function ($scope) {
-        $scope.test="hello World";
+    var app=angular.module('githubViewer',[]);
+
+    var MainController=function ($scope,github,$interval) {
+
+        var onUserComplete=function (data) {
+            $scope.user=data;
+           github.getRepos($scope.user)
+                .then(onRepos)
+                .catch(onError);
+        };
+
+        var onRepos=function (data) {
+          $scope.repos=data;
+        };
+
+        var onError=function (reason) {
+          $scope.error="Could not fetch the user";
+        };
+        var decrementCountdown=function () {
+            $scope.countdown--;
+            if($scope.countdown<1){
+                $scope.search($scope.username);
+            }
+        }
+        var countdownInterval=null;
+        var startCountdown=function () {
+           countdownInterval= $interval(decrementCountdown,1000,$scope.countdown);
+        }
+
+        $scope.search=function (username) {
+            github.getUser(username)
+                .then(onUserComplete)
+                .catch(onError);
+            if(countdownInterval){
+                $interval.cancel(countdownInterval);
+                $scope.countdown=null;
+            }
+        }
+        $scope.message="GitHub Viewer";
+        $scope.username="angular";
+        $scope.repoSortOrder="-stargazers_count";
+        $scope.countdown=5;
+        startCountdown();
     }
 
-    var mainModule=angular.module('mainModule',[]);
-    mainModule.controller('mainController',mainController)
+    app.controller('MainController',MainController)
 
 })();
